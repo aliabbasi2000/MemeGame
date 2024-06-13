5e965ed6637cafb1d703bbaa69660f515fed1372
Ali Abbasi <s323638@studenti.polito.it>
2024-06-13 03:55:43 +0200

    initialize the project: add packages / add navbar to the front end / add authentication (with Error) / Remove CSS / add Login, Logout, getUser APIs (with Error) / add userDAO

---
+++ server/server.mjs
@@ -0,0 +1,99 @@
+// import
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import {check, validationResult} from 'express-validator';
+import {getUser} from './user-dao.mjs';
+
+// Passport-related imports -- NEW
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import session from 'express-session';
+
+// init
+const app = express();
+const port = 3001;
+
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+// set up and enable CORS -- UPDATED
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200,
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+// Passport: set up local strategy -- NEW
+passport.use(new LocalStrategy(async function verify(username, password, cb) {
+  const user = await getUser(username, password);
+  if(!user)
+    return cb(null, false, 'Incorrect username or password.');
+    
+  return cb(null, user);
+}));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (user, cb) { // this user is id + email + name
+  return cb(null, user);
+  // if needed, we can do extra check here (e.g., double check that the user is still in the database, etc.)
+});
+
+const isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({error: 'Not authorized'});
+}
+
+app.use(session({
+  secret: "shhhhh... it's a secret!",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.authenticate('session'));
+
+/* ROUTES */
+
+
+// POST /api/sessions -- NEW
+app.post('/api/sessions', function(req, res, next) {
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+      if (!user) {
+        // display wrong login messages
+        return res.status(401).send(info);
+      }
+      // success, perform the login
+      req.login(user, (err) => {
+        if (err)
+          return next(err);
+        
+        // req.user contains the authenticated user, we send all the user info back
+        return res.status(201).json(req.user);
+      });
+  })(req, res, next);
+});
+
+// GET /api/sessions/current -- NEW
+app.get('/api/sessions/current', (req, res) => {
+  if(req.isAuthenticated()) {
+    res.json(req.user);}
+  else
+    res.status(401).json({error: 'Not authenticated'});
+});
+
+// DELETE /api/session/current -- NEW
+app.delete('/api/sessions/current', (req, res) => {
+  req.logout(() => {
+    res.end();
+  });
+});
+
+// far partire il server
+app.listen(port, () => { console.log(`API server started at http://localhost:${port}`); });
