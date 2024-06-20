e0bb137c07ff11513af4717603d4a2754b0aa49c
Ali Abbasi <ali.abbasi797979@gmail.com>
2024-06-20 19:17:58 +0200

    -add Profile component -use useRef to fix the Problem of fetching twice in memeGame Component

--- client/src/components/NavHeader.jsx
+++ client/src/components/NavHeader.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { Container, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { LogoutButton } from './AuthComponents';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 function NavHeader (props) {
@@ -8,11 +10,20 @@ function NavHeader (props) {
     <Navbar bg='primary' data-bs-theme='dark'>
       <Container fluid>
         <Link to='/' className='navbar-brand'>Meme Game</Link>
-        {/* NEW */}
+
         {props.loggedIn ? 
-          <LogoutButton logout={props.handleLogout} /> :
-          <Link to='/login'className='btn btn-outline-light'>Login</Link>
+            <React.Fragment>
+            {/* Profile icon */}
+            <Link to='/profile' className='nav-link link-light text-decoration-none'>
+              <i className='bi bi-person-circle'></i>
+            </Link>
+            {/* Logout button */}
+            <LogoutButton logout={props.handleLogout} />
+          </React.Fragment>
+          :
+          <Link to='/login' className='btn btn-outline-light'>Login</Link>
         }
+
       </Container>
     </Navbar>
   );
