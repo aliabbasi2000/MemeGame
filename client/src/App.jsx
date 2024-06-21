34d31d2a30d9bbed9a757018f6a66bbfab686c41
Ali Abbasi <ali.abbasi797979@gmail.com>
2024-06-21 17:02:13 +0200

    Change the layout of home page / Add Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import API from './API.mjs';
 import MemeGame from './components/MemeGame';
 import Game from "./components/Game";
 import Profile from './components/Profile';
+import Footer from './components/Footer';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -55,45 +56,57 @@ function App() {
 
 
   return (
+
+    <div className="App">
+      <NavHeader loggedIn={loggedIn} handleLogout={handleLogout} />
+      <Container fluid className='mt-3'>
+
+      {/* Conditional Rendering: If message is truthy(not null, undefined, false,), it will render the content.*/}
+      {message && <Row>
+          <Alert variant={message.type} onClose={() => setMessage('')} dismissible>{message.msg}</Alert>
+        </Row> }
+  
     <Routes>
-      <Route element={
-        <>
-        <NavHeader loggedIn={loggedIn} handleLogout={handleLogout} />
-        <Container fluid className='mt-3'>
+      <Route path="/" element={
+        <Container className='d-flex flex-column justify-content-start align-items-center vh-100'>
+          <Row className='justify-content-center mt-5'>
+            <Col className='text-center'>
+              
+            {!loggedIn && <h1  className="h2">What Do You Meme? </h1>}
+            {!loggedIn && <p> Get ready to test your creativity and humor!! <br/> Click the Button below </p>}
+            {!loggedIn && <Button variant="primary" onClick={startDemoGame} className='mt-3'>Start demo Game</Button>}
 
-          {/* Conditional Rendering: If message is truthy(not null, undefined, false,), it will render the content.*/}
-          {message && <Row>
-            <Alert variant={message.type} onClose={() => setMessage('')} dismissible>{message.msg}</Alert>
-          </Row> }
-
-          <Outlet/>
-        </Container>
-        </>
-      }>
-
-<Route path="/" element={
-          <Container className='d-flex flex-column justify-content-start align-items-center vh-100'>
-            <Row className='justify-content-center mt-5'>
-              <Col className='text-center'>
-                <h1  className="h3">Welcome to the MemeGame! developed by <a href="https://github.com/aliabbasi2000" target="_blank" rel="noopener noreferrer">Ali Abbasi</a></h1>
-                {!loggedIn && <p>Click the button below to start demo playing</p>}
-                {loggedIn && <p>Click the button below to start the Game!</p>}
-                {!loggedIn && <Button variant="primary" onClick={startDemoGame} className='mt-3'>Start demo Game</Button>}
-                {loggedIn && <Button variant="primary" onClick={startGame} className='mt-3'>Let the Memes Begin!</Button>}
-              </Col>
-            </Row>
-          </Container>
-        } />
-        <Route path="/demogame" element={<MemeGame />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="*" element={ <NotFound/> } />
-        <Route path='/login' element={
-          loggedIn ? <Navigate replace to='/' /> : <LoginForm login={handleLogin} />
-        } />
-        <Route path='/profile' element = {<Profile />}/>
+            {loggedIn && <h1  className="h3">Welcome to the Meme Game! </h1>}
+            {loggedIn && <p>Click the button below to start the Real Game!</p>}
+            {loggedIn && <Button variant="primary" onClick={startGame} className='mt-3'>Let the Memes Begin</Button>}
             
-      </Route>
+            </Col>
+          </Row>
+        </Container>
+      } />
+
+      <Route path="/demogame" element={<MemeGame />} />
+
+      {/* If loggedIn is false, redirect to home page; otherwise, render the Game component. */} 
+      {/* For the case that user clicks on log out during the game */}
+      <Route path='/game' element={
+        !loggedIn ? <Navigate replace to='/' /> : <Game />
+      } />
+      <Route path="*" element={ <NotFound/> } />
+
+      {/* If loggedIn is true, redirect to home page; otherwise, render the LoginForm component. */}
+      <Route path='/login' element={
+        loggedIn ? <Navigate replace to='/' /> : <LoginForm login={handleLogin} />
+      } />
+
+      {/* If loggedIn is true, render the Profile component; otherwise, redirect to home page. */}
+      <Route path='/profile' element={loggedIn ? <Profile /> : <Navigate to='/' />} />
+
+      
     </Routes>
+    </Container>
+    <Footer />
+    </div>
 
   )
 }
